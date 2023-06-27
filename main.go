449d4f7b5815f07013819cd1e4a30306bc1d93dd
azwan11 <azwanalkhawarismi@gmail.com>
2023-06-27 10:31:42 +0700

    yok gas

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+const DSN = "root:@/toko_buku?charset=utf8&parseTime=True&loc=Local"
+
+type Buku struct {
+	gorm.Model
+	Judul     string `json:"judul" `
+	Pengarang string `json:"pengarang"`
+	Penerbit  string `json:"penerbit"`
+}
+
+func InitDB() {
+	var err error
+	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// func InitialMigration() {
+// 	DB.AutoMigrate(&Buku{})
+// }
+
+func GetBooks(context echo.Context) error {
+	var buku []Buku
+	if err := DB.Find(&buku).Error; err != nil {
+		return err
+	}
+	return context.JSON(http.StatusOK, buku)
+}
+
+func CreateBooks(context echo.Context) error {
+	var params Buku
+	err := context.Bind(&params)
+	if err != nil {
+		return err
+	}
+
+	if err := DB.Create(&params).Error; err != nil {
+		return err
+	}
+	return context.JSON(http.StatusCreated, params)
+}
+
+func main() {
+	InitDB()
+	// InitialMigration()
+
+	e := echo.New()
+
+	// Routing
+	e.GET("/books", GetBooks)
+	e.POST("/create-books", CreateBooks)
+
+	e.Start(":8000")
+}
